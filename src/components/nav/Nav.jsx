f3b6201753444d423571a2f6e04fe92bee998184
puranchandralohar <clpuran95@gmail.com>
2023-01-02 14:00:53 +0530

    mui added in nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,35 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/system";
 
-import './nav.css'
+import "./nav.css";
 
 const links = [
   {
-      name: 'Home',
-      to: '/',
-      active: 'home'
+    name: "Home",
+    to: "/",
+    active: "home",
   },
   {
-      name: 'About Me',
-      to: '/about',
-      active: 'about'
+    name: "About Me",
+    to: "/about",
+    active: "about",
   },
   {
-      name: 'Projects',
-      to: '/projects',
-      active: 'projects'
-  }
-]
+    name: "Projects",
+    to: "/projects",
+    active: "projects",
+  },
+];
 
 export const Nav = () => {
-
   return (
-    <nav>
-      {links.map((link,index)=>{
-          return(
-            <Link to={link.to} key={index}>{link.name}</Link>
-          )
-      })}
-    </nav>
-  )
-}
+    <Box component={"nav"} width={"100%"}>
+      <Box
+        component={"ul"}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "5rem",
+        }}
+      >
+        {links.map((link, index) => {
+          return (
+            <Box
+              component={"li"}
+              key={index}
+              sx={{
+                listStyle: "none",
+              }}
+            >
+              <Link to={link.to} style={{ textDecoration: "none" }}>
+                {link.name}
+              </Link>
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+};
